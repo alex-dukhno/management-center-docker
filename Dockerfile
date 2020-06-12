2358cf81570ba81f68d12b7e87d02be35990a94d
Iurii Kutelmakh <iuriikutelmakh@Iuriis-MacBook-Pro.local>
2020-06-12 15:43:25 +0300

    Updated docker file

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM openjdk:11-jre-slim-sid
 
 ENV MC_VERSION 4.1-ALPHA-2
-ENV MC_HOME /opt/hazelcast/mancenter
+ENV MC_HOME /opt/hazelcast/management-center
 ENV MC_DATA /data
 
 ENV MC_HTTP_PORT 8080
 ENV MC_HTTPS_PORT 8443
 ENV MC_HEALTH_CHECK_PORT 8081
-ENV MC_CONTEXT hazelcast-mancenter
+ENV MC_CONTEXT_PATH /
 
 ARG MC_INSTALL_NAME="hazelcast-management-center-${MC_VERSION}"
 ARG MC_INSTALL_ZIP="${MC_INSTALL_NAME}.zip"
@@ -15,7 +15,7 @@ ARG MC_INSTALL_WAR="hazelcast-management-center-${MC_VERSION}.jar"
 
 ENV MC_RUNTIME "${MC_HOME}/${MC_INSTALL_WAR}"
 
-# Install wget to download management center
+# Install wget to download Management Center
 RUN apt-get update \
  && apt-get install --no-install-recommends --yes \
       wget unzip \
@@ -35,8 +35,9 @@ RUN unzip ${MC_INSTALL_ZIP} \
  && rm -rf ${MC_INSTALL_NAME}
 
 # Runtime environment variables
-ENV JAVA_OPTS_DEFAULT "-Dhazelcast.mancenter.home=${MC_DATA} -Djava.net.preferIPv4Stack=true"
+ENV JAVA_OPTS_DEFAULT "-Dhazelcast.mc.home=${MC_DATA} -Djava.net.preferIPv4Stack=true"
 
+ENV NO_CONTAINER_SUPPORT "false"
 ENV MIN_HEAP_SIZE ""
 ENV MAX_HEAP_SIZE ""
 
@@ -45,6 +46,9 @@ ENV MC_INIT_SCRIPT ""
 ENV MC_INIT_CMD ""
 
 ENV MC_CLASSPATH ""
+
+ENV MC_ADMIN_USER ""
+ENV MC_ADMIN_PASSWORD ""
 
 COPY files/mc-start.sh /mc-start.sh
 RUN chmod +x /mc-start.sh
--- files/mc-start.sh
+++ files/mc-start.sh
@@ -7,12 +7,17 @@ else
     export JAVA_OPTS="${JAVA_OPTS_DEFAULT}"
 fi
 
-if [ -n "${MIN_HEAP_SIZE}" ]; then
-    export JAVA_OPTS="${JAVA_OPTS} -Xms${MIN_HEAP_SIZE}"
-fi
-
-if [ -n "${MAX_HEAP_SIZE}" ]; then
-    export JAVA_OPTS="${JAVA_OPTS} -Xmx${MAX_HEAP_SIZE}"
+if [ "${CONTAINER_SUPPORT:-true}" = "false" ] ;then
+    echo "using manual heap sizing by specifying MIN_HEAP_SIZE or MAX_HEAP_SIZE or custom settings configure by JAVA_OPTS"
+    if [ -n "${MIN_HEAP_SIZE}" ]; then
+        export JAVA_OPTS="${JAVA_OPTS} -Xms${MIN_HEAP_SIZE}"
+    fi
+    if [ -n "${MAX_HEAP_SIZE}" ]; then
+        export JAVA_OPTS="${JAVA_OPTS} -Xmx${MAX_HEAP_SIZE}"
+    fi
+else
+    echo "using automatic sizing of heap size by up to 80% of available memory and starting with container support"
+    export JAVA_OPTS="${JAVA_OPTS} -XX:+UseContainerSupport -XX:MaxRAMPercentage=80"
 fi
 
 if [ -n "${MC_CLASSPATH}" ]; then
@@ -22,20 +27,28 @@ else
 fi
 
 if [ -n "${MC_INIT_CMD}" ]; then
-   echo "executing command specified by MC_INIT_CMD"
+   echo "executing command specified by MC_INIT_CMD for container initialization"
    eval "${MC_INIT_CMD}"
 fi
 
 if [ -n "${MC_INIT_SCRIPT}" ]; then
-   echo "loading script $MC_INIT_SCRIPT specified by MC_INIT_SCRIPT"
-   source ${MC_INIT_SCRIPT}
+    echo "loading script $MC_INIT_SCRIPT specified by MC_INIT_SCRIPT for container initialization"
+    source ${MC_INIT_SCRIPT}
 fi
 
-echo "########################################"
-echo "# JAVA_OPTS=${JAVA_OPTS}"
-echo "# MC_CLASSPATH=${MC_CLASSPATH}"
-echo "# starting now...."
-echo "########################################"
+if [ -n "${MC_ADMIN_USER}" ] && [ -n "${MC_ADMIN_PASSWORD}" ]; then
+  echo "Creating admin user"
+  source ./mc-conf.sh user create -H=${MC_DATA} -n=${MC_ADMIN_USER} -p=${MC_ADMIN_PASSWORD} -r=admin
+  if [ $? -eq 0 ]; then
+    echo "User created successfully"
+  else
+    exit 1
+  fi
+fi
+
+echo "##################################################"
+echo "# initialisation complete, starting now...."
+echo "##################################################"
 
 # --add-opens flag is required to prevent this issue: https://jira.spring.io/browse/SPR-15859
 set -x
@@ -43,7 +56,7 @@ exec java \
     --add-opens java.base/java.lang=ALL-UNNAMED \
     -server ${JAVA_OPTS} \
     -cp "${MC_CLASSPATH}" \
-    Launcher \
-    ${MC_HTTP_PORT} \
-    ${MC_HTTPS_PORT} \
-    ${MC_CONTEXT}
+    -Dhazelcast.mc.contextPath=${MC_CONTEXT_PATH} \
+    -Dhazelcast.mc.http.port=${MC_HTTP_PORT} \
+    -Dhazelcast.mc.https.port=${MC_HTTPS_PORT} \
+    com.hazelcast.webmonitor.Launcher
